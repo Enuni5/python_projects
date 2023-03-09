196f1ac83672d7b04efa9fbf6bd51a1bac5e9dc8
Enuni5 <emilio.nuni@gmail.com>
2023-03-09 22:33:06 +0100

    Adding 00_hello.py
    This is my very first file in python coding language.
    I hope I can learn a lot from this.

---
+++ 00_hello.py
@@ -0,0 +1,27 @@
+# Hello python
+
+print("Hello python")
+
+# Discovering some system functions and other prints
+
+print(type(5))
+print(type(True))
+print(3 + 5)
+
+# Messing around 
+
+print("       1")
+print("     2 3")
+print("   4 5 6")
+print("7 8 9 10")
+
+"""
+This is nice.
+Is this how you make multiple lines 
+comments?
+Indeed it is.
+"""
+
+# Concatenating strings
+
+print("hello"+","+" world")
